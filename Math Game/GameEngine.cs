301e4eb8e517e8839daecdbf8ac53234f5670a0d
Scrunchyfish <124253949+Scrunchyfish@users.noreply.github.com>
2024-01-23 20:27:21 +1030

    refactored into classes histoery now displays correct or incorrect

---
+++ Math Game/GameEngine.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Game
+{
+    
+    internal class GameEngine
+    {
+        Random rand = new Random();
+        internal List<string> history = new();
+        internal void AdditionGame()
+        {
+            int score = 0;
+            bool correct = true;
+            string? readValueAdd = "";
+            Console.WriteLine("Welcome to Addition mode. Type exit to return to the main menu.");
+            while (readValueAdd.ToLower() != "exit")
+            {
+
+                int a = rand.Next(0, 100), b = rand.Next(0, 100);
+                int ans = 0;
+                Console.WriteLine($"{a} + {b} =");
+
+                readValueAdd = Console.ReadLine();
+                if (readValueAdd.ToLower() == "exit")
+                    break;
+
+                if (int.TryParse(readValueAdd, out ans) && ans == a + b)
+                {
+                    score++;
+                    Console.WriteLine($"Correct! Score: {score}");
+                    correct = true;
+
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect, try again. Score: {score}");
+                    correct = false;
+                }
+                history.Add($"{a} + {b} = {readValueAdd} - {(correct ? "correct": "incorrect")}");
+            }
+        }
+
+        internal void SubtractionGame()
+        {
+            int score = 0;
+            string? readValueAdd = "";
+            Console.WriteLine("Welcome to Subtraction mode. Type exit to return to the main menu.");
+            while (readValueAdd.ToLower() != "exit")
+            {
+
+                int a = rand.Next(0, 100), b = rand.Next(0, 100);
+                int ans = 0;
+                Console.WriteLine($"{a} - {b} =");
+
+                readValueAdd = Console.ReadLine();
+                if (readValueAdd.ToLower() == "exit")
+                    break;
+
+                if (int.TryParse(readValueAdd, out ans) && ans == a - b)
+                {
+                    score++;
+                    Console.WriteLine($"Correct! Score: {score}");
+
+
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect, try again. Score: {score}");
+                }
+                history.Add($"{a} - {b} = {readValueAdd}");
+            }
+        }
+
+        internal void MultiplicationGame()
+        {
+            int score = 0;
+            string? readValueAdd = "";
+            Console.WriteLine("Welcome to Multiplication mode. Type exit to return to the main menu.");
+            while (readValueAdd.ToLower() != "exit")
+            {
+
+                int a = rand.Next(0, 10), b = rand.Next(0, 10);
+                int ans = 0;
+                Console.WriteLine($"{a} * {b} =");
+
+                readValueAdd = Console.ReadLine();
+                if (readValueAdd.ToLower() == "exit")
+                    break;
+
+                if (int.TryParse(readValueAdd, out ans) && ans == a * b)
+                {
+                    score++;
+                    Console.WriteLine($"Correct! Score: {score}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect, try again. Score: {score}");
+                }
+                history.Add($"{a} * {b} = {readValueAdd}");
+            }
+        }
+
+        internal void DivisionGame()
+        {
+            int score = 0;
+            string? readValueAdd = "";
+            Console.WriteLine("Welcome to Division mode. Type exit to return to the main menu.");
+            while (readValueAdd.ToLower() != "exit")
+            {
+
+                int a = rand.Next(1, 100), b = rand.Next(1, 100);
+                int ans = 0;
+                if (a % b == 0)
+                    Console.WriteLine($"{a} / {b} =");
+                else continue;
+
+                readValueAdd = Console.ReadLine();
+                if (readValueAdd.ToLower() == "exit")
+                    break;
+
+                if (int.TryParse(readValueAdd, out ans) && ans == a / b)
+                {
+                    score++;
+                    Console.WriteLine($"Correct! Score: {score}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect, try again. Score: {score}");
+                }
+                history.Add($"{a} / {b} = {readValueAdd}");
+            }
+        }
+    }
+}
---
+++ Math Game/Menu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Game
+{
+    internal class Menu
+    {
+        internal GameEngine gameEngine = new GameEngine();
+
+        public void RunMenu()
+        {
+            string? readValue;
+
+            do
+            {
+                Console.WriteLine("Welcome to the maths game.\n");
+                Console.WriteLine("Choose what mode you want to play.");
+                Console.WriteLine("1 - Addition");
+                Console.WriteLine("2 - Subtraction");
+                Console.WriteLine("3 - Multiplication");
+                Console.WriteLine("4 - Division");
+                Console.WriteLine("5 - History");
+                readValue = Console.ReadLine();
+
+                switch (readValue)
+                {
+                    case "1":
+                        gameEngine.AdditionGame();
+                        break;
+
+                    case "2":
+                        gameEngine.SubtractionGame();
+                        break;
+
+                    case "3":
+                        gameEngine.MultiplicationGame();
+                        break;
+
+                    case "4":
+                        gameEngine.DivisionGame();
+                        break;
+
+                    case "5":
+                        foreach (string s in gameEngine.history)
+                            Console.WriteLine(s);
+                        break;
+
+                }
+            } while (readValue.ToLower() != "exit");
+        }
+    }
+}
